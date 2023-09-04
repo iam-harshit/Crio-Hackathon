0ffa69a3bf3a86e4858df960a6ab8bd485ab4517
iam-harshit <importantemail545@gmail.com>
2023-09-04 18:33:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import BifurcationPlayer from "./components/BifurcationPlayer";
+import video from "./asset/video-1.mp4";
+
 const App = () => {
+  const videoUrl = video;
+  const videoChapters = [
+    { title: "What is computer?", start: 5.0, end: 6.0 },
+    { title: "Why we need to use computer?", start: 0.4, end: 0.7 },
+  ];
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <BifurcationPlayer url={videoUrl} chapters={videoChapters} />
     </div>
   );
 }
