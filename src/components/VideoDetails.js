7f2f9d425e868c688f5ede692ac58ef021e5c2e4
Varsha Singh <95873244+Varsha2630@users.noreply.github.com>
2023-09-05 18:07:11 +0530

    Remove extra width from the video player

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -3,20 +3,19 @@ import SuggestionVideoCard from "./SuggestionVideoCard";
 import VideoCard from "./VideoCard";
 
 const VideoDetails = ({ url, chapters }) => {
-
   return (
-    <div className="flex justify-center flex-row h-[calc(100%-40)] bg-white">
-      <div className="w-full max-w-[1280px] flex flex-col lg:flex-row">
+    <div className="flex justify-center flex-row h-[calc(100vh-40px)] bg-white">
+      <div className="w-full max-w-screen-xl flex flex-col lg:flex-row">
         <div className="flex flex-col lg:w-[calc(100%-350px)] xl:w-[calc(100%-400px)] px-4 py-3 lg:py-6 overflow-y-auto">
-          <div className="h-[200px] md:h-[400px] lg:h-[400px] xl:h-[550px] ml-[-16px] lg:ml-0 mr-[-16px] lg:mr-0">
+          <div className="h-[200px] md:h-[400px] lg:h-[400px] xl:h-[420px] ml-[-16px] lg:ml-0 mr-[-16px] lg:mr-0">
             <VideoCard url={url} chapters={chapters} />
           </div>
-          <div className="text-black font-bold text-sm md:text-xl line-clamp-2">
-            Hello this is a sample video
+          <div className="text-black font-bold text-sm md:text-xl line-clamp-2 mt-4 md:mt-6">
+            Hello, this is a sample video
           </div>
         </div>
         <div className="flex flex-col py-6 px-4 overflow-y-auto lg:w-[350px] xl:w-[400px]">
-           <SuggestionVideoCard/>
+          <SuggestionVideoCard />
         </div>
       </div>
     </div>
